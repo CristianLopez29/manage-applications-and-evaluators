ca3598a55b6149cfd8081cdb79ea814c4b5123b6
CristianLopez29 <90840561+CristianLopez29@users.noreply.github.com>
2026-02-18 23:18:58 +0100

    chore: Enable Laravel Telescope for monitoring (#24)

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Src\Candidates\Infrastructure\CandidatesServiceProvider::class,
     Src\Evaluators\Infrastructure\EvaluatorsServiceProvider::class,
 ];
